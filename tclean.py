1f7dd3e73f5835d03238f93938d3a2168d85561e
Chad R. Miller <chad.r.miller@gmail.com>
2026-01-05 15:19:59 +0000

    Tweaked Gotify settings in TClean

--- tclean.py
+++ tclean.py
@@ -31,9 +31,9 @@ free_space = client.get_free_space().result
 print(f"🤔 Current Free Space: \n    {(free_space / GB):,.2f} Gb")
 if free_space >= DESIRED_FREE_SPACE:
     print(f"😊  The torrent server has ample free space!")
-    gotify.create_message(f"Desired: {DESIRED_FREE_SPACE / GB:,.2f}\nFree space: {(free_space / GB):,.2f} Gb,\nNothing to do. 😊", title="TClean", priority=0)
+    gotify.create_message(f"Desired: {DESIRED_FREE_SPACE / GB:,.2f}\nFree space: {(free_space / GB):,.2f} Gb,\nNothing to do. 😊", title="TClean", priority=5)
 else:
-    gotify.create_message(f"Desired: {DESIRED_FREE_SPACE / GB:,.2f}\nFree space: {(free_space / GB):,.2f} Gb,\nSearching for torrents to delete...", title="TClean", priority=0)
+    gotify.create_message(f"Desired: {DESIRED_FREE_SPACE / GB:,.2f}\nFree space: {(free_space / GB):,.2f} Gb,\nSearching for torrents to delete...", title="TClean", priority=5)
     all_torrents = client.get_torrents_status(
        keys=['seeding_time', 'hash', 'is_finished', 'paused', 'total_peers', 'total_seeds', 'ratio', 'name',
              'time_since_transfer', 'label','total_size']
@@ -48,16 +48,16 @@ else:
     # So now we need to sort them from high to low on time_since_transfer
     sorted_candidates = sorted(list(candidates.items()),key=sort_by_most_recent_used,reverse=True)
     print(f"There are {len(sorted_candidates)} torrents eligible for removal:")
-    gotify.create_message(f"{len(sorted_candidates)} torrents eligible.", title="TClean", priority=0)
+    gotify.create_message(f"{len(sorted_candidates)} torrents eligible.", title="TClean", priority=5)
     for index in range(0, len(sorted_candidates)):
         print(f"{index + 1}. {sorted_candidates[index][1]['name']} -- Size: {sorted_candidates[index][1]['total_size'] / GB:,.2f} Gb -- Torrent ID: {sorted_candidates[index][0]}")
         # Code to delete a torrent:
         success = client.remove_torrent(torrent_id=sorted_candidates[index][0], remove_data=True)
-        gotify.create_message(f"Torrent Deleted:\n{sorted_candidates[index][1]['name']}\nFreed space: {sorted_candidates[index][1]['total_size'] / GB:,.2f} Gb")
+        gotify.create_message(f"Torrent Deleted:\n{sorted_candidates[index][1]['name']}\nFreed space: {sorted_candidates[index][1]['total_size'] / GB:,.2f} Gb",title="TClean",priority=5)
         while free_space != client.get_free_space().result:
             free_space = client.get_free_space().result
             time.sleep(5) # wait for free space to stabilize -- it might've been a huge torrent.  If there are active torrents, this might take a while.  That's okay.
         if free_space >= DESIRED_FREE_SPACE:
             break
-    gotify.create_message(f"TClean complete.\nFree space: {free_space:,.2f} Gb", title="TClean", priority=0)
+    gotify.create_message(f"TClean complete.\nFree space: {free_space:,.2f} Gb", title="TClean", priority=5)
 
